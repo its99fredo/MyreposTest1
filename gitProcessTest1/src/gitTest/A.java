bb9ad76ca1c3c38f67ed877ccd9dc5c3d042c51c
scson <scson@DESKTOP-7PGQ8AQ>
2024-12-29 20:08:55 +0900

    A, B 출력문 추가

--- gitProcessTest1/src/gitTest/A.java
+++ gitProcessTest1/src/gitTest/A.java
@@ -3,6 +3,7 @@ package gitTest;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("A class");
+		System.out.println("devB branch");
 		System.out.println("Hello World!");
 		}
 }
--- gitProcessTest1/src/gitTest/B.java
+++ gitProcessTest1/src/gitTest/B.java
@@ -3,6 +3,7 @@ package gitTest;
 public class B {
 	public static void main(String[] args) {
 		System.out.println("B class");
+		System.out.println("devB branch");
 		System.out.println("Hello World!");
 		}
 }
