91d0666d4c1a8b8b65dd5d37d03fa9f15ca8676d
scson <scson@DESKTOP-7PGQ8AQ>
2024-12-29 19:42:47 +0900

    v0.0

---
+++ gitProcessTest1/src/gitTest/B.java
@@ -0,0 +1,9 @@
+package gitTest;
+
+public class B {
+	public static void main(String[] args) {
+		System.out.println("B class");
+		System.out.println("Hello World!");
+		}
+
+}
---
+++ gitProcessTest1/src/gitTest/C.java
@@ -0,0 +1,8 @@
+package gitTest;
+
+public class C {
+	public static void main(String[] args) {
+		System.out.println("C class");
+		System.out.println("Hello World!");
+		}
+}
