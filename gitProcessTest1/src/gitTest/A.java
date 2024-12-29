b6a34c1b33229fcb6aefd029f7b526b8f9930980
scson <scson@DESKTOP-7PGQ8AQ>
2024-12-29 20:10:43 +0900

    A 테스트 출력문 수정

--- gitProcessTest1/src/gitTest/A.java
+++ gitProcessTest1/src/gitTest/A.java
@@ -3,6 +3,6 @@ package gitTest;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("A class");
-		System.out.println("Hello World!");
+		System.out.println("Hello Git!");
 		}
 }
