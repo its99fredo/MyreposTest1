354d8ddd5f05ac694eb4586b70d7c2bb0c1c303f
scson <scson@DESKTOP-7PGQ8AQ>
2024-12-29 19:53:22 +0900

    add branch devC

--- gitProcessTest1/src/gitTest/C.java
+++ gitProcessTest1/src/gitTest/C.java
@@ -3,6 +3,7 @@ package gitTest;
 public class C {
 	public static void main(String[] args) {
 		System.out.println("C class");
+		System.out.println("devC branch process");
 		System.out.println("Hello World!");
 		}
 }
